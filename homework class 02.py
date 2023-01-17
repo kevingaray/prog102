60c1aeeeb3de7844b38e8283765a5a5b38dfa0bd
kevin_gerardo <kevingerardo23@gmail.com>
2023-01-17 16:00:24 -0500

    branch created homework class 02

--- homework class 02.py
+++ homework class 02.py
@@ -1,7 +1,6 @@
-# Write a short Python script, that takes two numbers values, 
-# where one of them is the hours worked by employee and the other is the rate,
-# the equation to calculate the payment is (hours * rate), but if the hours 
-# is more than 40 the equation is (hours * rate + (hours - 40) * rate * 0.5). 
+# Write a short Python script, that takes two numbers values, where one of them is the hours worked 
+# by employee and the other is the rate, the equation to calculate the payment is (hours * rate), 
+# but if the hours is more than 40 the equation is (hours * rate + (hours - 40) * rate * 0.5). 
 # So we need a script to do this automatically.
 
 hours = int(input("Insert the hours : "))
