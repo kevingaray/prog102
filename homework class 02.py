374a1f677a41b6dbb0b61d6f7b22e6d4834d5405
kevin_gerardo <kevingerardo23@gmail.com>
2023-01-18 16:26:33 -0500

    update open file

--- homework class 02.py
+++ homework class 02.py
@@ -3,13 +3,24 @@
 # but if the hours is more than 40 the equation is (hours * rate + (hours - 40) * rate * 0.5). 
 # So we need a script to do this automatically.
 
-hours = int(input("Insert the hours : "))
-rate = int(input("Insert the rate : "))
+# hours = int(input("Insert the hours : "))
+# rate = int(input("Insert the rate : "))
 
-if hours < 40:
-    payment = (hours*rate)
-else:
-    payment = (hours * rate + (hours - 40) * rate * 0.5)
+# file = open('employees.csv')
 
-print(f"Payment: {payment}")
+with open('employees.csv') as f:
+    next(f) # skip header line
+    for line in f:
+        if line != '\n':
+            name,hours,rate=line.split(",")
+            hours = int(hours)
+            rate = float(rate)
+
+            if hours < 40:
+                payment = (hours*rate)
+            else:
+                payment = (hours * rate + (hours - 40) * rate * 0.5)
+            
+            print(f"{name}: {payment}")
+        
 
